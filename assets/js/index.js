4d75112ed3f2f094df81cfbf0b7ecb4923430641
Modern Viking <thomascustoms715@gmail.com>
2019-12-13 22:20:10 -0700

    setting first few functions and questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,12 +10,12 @@ let controlsEl = $('#controls');
 let scoreBtnEl = $('#score-btn');
 let scoreEl = $('#score-container')
 let clearBtnEl = $('#clr-btn');
+let timeEl = $('#time');
 
     // Base Variables
 let questionIndex = 0;
-let currentQ = questionsArr[questionIndex];
+let questions = questionsList[questionIndex];
 let score = 0;
-let interval;
 
     // Click Listeners
 startButtonEl.click(startQuiz);
@@ -24,3 +24,59 @@ nextBtnEl.click(nextQuestion);
 submitBtnEl.click(submitScore);
 scoreBtnEl.click(viewScores);
 clearBtnEl.click(clearScores);
+
+    // Functions
+
+function startQuiz() {
+    questionIndex = 0;
+    questions = questionsArr[questionIndex];
+    startButtonEl.addClass('hide');
+    scoreBtnEl.addClass('hide');
+    qContainerEl.removeClass('hide');
+    clearBtnEl.addClass('hide');
+    dispQuestion(questions);
+    score = 90;
+    timeEl.text(`Time Left: ${score}`);
+    startTimer()
+
+};
+
+function dispQuestion(question) {
+    aGridEl.empty();
+    questionEl.text(question.title);
+    $.each(question.choices, function (index, options) {
+        let newButton = $('<button>');
+        newButton.text(options);
+        newButton.addClass('btn');
+        aGridEl.append(newButton);
+    })
+};
+
+function selectAnswer(e) {
+     if ($(e.target).text() === questions.answer) {
+         $(e.target).addClass('btn-success');
+         nextBtnEl.removeClass('hide');
+     } else if ($(e.target).text() !== questions.answer && $(e.target).hasClass('btn')){
+        $(e.target).addClass('btn-danger');
+        score -= 15;
+        timeEl.text(`Time Left: ${score}`)
+        timeEl.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+     }
+};
+
+function nextQuestion () {
+    questionIndex++
+    questions = questionsArr[questionIndex]
+    if (questionIndex === questionsArr.length){
+        stopTimer()
+        
+        qContainerEl.addClass('hide')
+        formEl.removeClass('hide');
+        submitBtnEl.removeClass('hide');
+        nextBtnEl.addClass('hide');
+        timeEl.text(`Points: ${score}`)
+    } else {
+        dispQuestion(questions);
+        nextBtnEl.addClass('hide');
+    }
+};
---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+var questionsList = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+      {
+          title: "Which of these is a JS library?",
+          choices: ["ES6", "React", "CSS", "EMCA"],
+          answer: "React"
+      },
+      {
+        title: "Which of these is a class target in CSS?",
+        choices: [".class", "#class", "<class>", ". Class"],
+        answer: ".class"
+      },
+      {
+        title: "DOM Stands for what?",
+        choices: ["Destination On Markup", "Document Oriented Module", "Direct Object Manipulation", "Document Object Model"],
+        answer: "Document Object Model"
+      },
+  
+    ];
